7b3597cc4d61bdea66d19583a6392750c032168f
Vlad Shorin <kniger33@gmail.com>
2018-12-27 14:26:40 +0300

    First commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CubeController : MonoBehaviour {
+
+    public GameObject playCube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playCube.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+	}
+
+    public void LoadSecondWindow()
+    {
+        SceneManager.LoadScene("MainWindow");
+    }
+}
